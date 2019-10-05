c238c926347ac1c861f4001e0913e73ee5da9e8a
Juan José Cacho <juanjo.cacho@evenbytes.com>
2019-10-05 13:50:54 +0200

    Añadido el análisis de lenguaje natural con Google NLP API

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gcp-bigquery-twitter-stream
 Stream Twitter Data into BigQuery with tweepy
+
+
+APP de twitter
+Crear proyecto
+Habilitar billin account
+Habilitar BigQuery
+- Definir schema
+Habilitar NLP API
+- Enable the required APIs and modify permissions
+Crear instancia ComputeEngine
+
+sudo apt update
+sudo apt install python python-dev python3 python3-dev python3-pip
+
+sudo python3 -m pip install tweepy google-api-python-client google-cloud google-cloud-vision google.cloud.bigquery google-cloud-language
+
+sudo chmod +x /home/juanjo_cacho/gettweets.py 
+
+sudo crontab -e
+
+*/10 * * * * cd /home/juanjo_cacho && ./gettweets.py > /tmp/stream.log 2>&1
+
+
+
--- gettwets.py
+++ gettwets.py
@@ -1,16 +1,19 @@
 import tweepy
 from tweepy.auth import OAuthHandler
 from google.cloud import bigquery
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
 #CONFIGURATION
 APP_KEY = ""
 APP_SECRET = ""
-OATH_TOKEN = "-OI8LWdsWPejepUmLj9e6YGpaCDzMNAQ"
+OATH_TOKEN = ""
 OATH_TOKEN_SECRET = ""
 BIGQUERY_DATESET = ""
 BIGQUERY_TABLE = ""
-BIGQUERY_QUERY_TABLE = "``"
-GCP_SERVICE_ACCOUNT_JSON = '';
+BIGQUERY_QUERY_TABLE = "`gcp-twitter-stream."+BIGQUERY_DATESET+"."+BIGQUERY_TABLE+"`"
+GCP_SERVICE_ACCOUNT_JSON = 'GCP-twitter-stream-f8f02ba293cc.json';
 HASHTAG = ""
 
 #BIGQUERY CONNECTION
@@ -21,6 +24,9 @@ table_ref = client.dataset(dataset_id).table(table_id)
 table = client.get_table(table_ref) 
 rows_to_insert = []
 last_id_saved = "";
+
+#NLP CONFIGURATION
+nlpClient = language.LanguageServiceClient().from_service_account_json(GCP_SERVICE_ACCOUNT_JSON)
 
 #TWITTER AUTHENTICATION
 auth = OAuthHandler(APP_KEY, APP_SECRET)
@@ -37,19 +43,27 @@ for tw in last:
 try:
     api.verify_credentials()
     print("Authentication OK")
-
-    tweets = tweepy.Cursor(api.search, q='\"' + HASHTAG + '\"', lang='es', since_id=last_id_saved).items(200)
-
-    for tweet in tweets:
-     #https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object.html
-     row = [str(tweet.created_at),tweet.id_str,tweet.text,tweet.source,tweet.user.id_str,tweet.user.name,tweet.user.screen_name,tweet.user.description,str(tweet.user.verified),str(tweet.user.created_at),str(tweet.user.geo_enabled),tweet.user.lang];
-     print(tweet.id_str + " " + tweet.user.name + "\n")
-     rows_to_insert.append(row)
-
-    errors = client.insert_rows(table, rows_to_insert)
-
-    assert errors == []
-
 except:
     print("Error during authentication")
+    
+tweets = tweepy.Cursor(api.search, q='\"' + HASHTAG + '\"', lang='es', since_id=last_id_saved).items(200)
+
+for tweet in tweets:
+    #https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object.html
+
+    #NLP analysis
+    document = types.Document(
+        content = tweet.text,
+        type = enums.Document.Type.PLAIN_TEXT)
+    sentiment = nlpClient.analyze_sentiment(document=document).document_sentiment;
+    
+    row = [tweet.id_str,str(tweet.created_at),tweet.text,tweet.source,tweet.user.id_str,tweet.user.name,tweet.user.screen_name,tweet.user.description,str(tweet.user.verified),str(tweet.user.created_at),str(tweet.user.geo_enabled),tweet.user.lang,str(sentiment.score), str(sentiment.magnitude)];
+    print(tweet.id_str + " @" + tweet.user.screen_name + "\n" + " / " + str(sentiment.score) + " / " + str(sentiment.magnitude))
+    rows_to_insert.append(row)
+
+errors = client.insert_rows(table, rows_to_insert)
+
+assert errors == []
+
+
 
